6320afcc7e80c6b6a38d34cd3a21dd753f6a69ed
hak7a3 <hak7a3@gmail.com>
2014-01-03 20:58:42 +0900

    are...?

--- package.sh
+++ package.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # script for packaging...
+zip -r lisp-on-tex.zip \
+  ./lisp-on-tex/LICENSE \
+  ./lisp-on-tex/README \
+  ./lisp-on-tex/*.sty \
+  ./lisp-on-tex/examples/*.tex \
+  ./lisp-on-tex/examples/*.pdf \
+  ./lisp-on-tex/tug2013
 
-zip -r lisp-on-tex.zip \ 
-  ../lisp-on-tex/LICENCE \
-  ../lisp-on-tex/README \
-  ../lisp-on-tex/*.sty \
-  ../lisp-on-tex/examples/*.tex \
-  ../lisp-on-tex/examples/*.pdf \
-  ../lisp-on-tex/tug2013/
